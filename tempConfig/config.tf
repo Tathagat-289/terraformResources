78b1571e459556a21d04d4123231e070010cd4a7
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2021-05-12 14:02:08 +0530

    added temp config file

---
+++ tempConfig/config.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hashicorp/azurerm"
+      version = "~> 2.12"
+    }
+    newrelic = {
+      # source is required for providers in other namespaces, to avoid ambiguity.
+      source  = "newrelic/newrelic"
+      version = "~> 2.1.1"
+    }
+  }
+}
+
+
+variable "tfmodule4" {}
+variable "slmodule4" {}
+
+
+resource "null_resource" "delaymodule41" {
+  provisioner "local-exec" {
+    command = var.tfmodule4
+    interpreter = ["/bin/sleep"]
+  }
+}
