3e4c5e199b9a2178b4a1e91ac7b84c82f3837ef5
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2021-05-12 14:18:17 +0530

    remove other resources than terraform

--- tempConfig/config.tf
+++ tempConfig/config.tf
@@ -6,16 +6,3 @@ terraform {
   }
   required_version = ">= 0.14"
 }
-
-
-
-variable "tfmodule4" {}
-variable "slmodule4" {}
-
-
-resource "null_resource" "delaymodule41" {
-  provisioner "local-exec" {
-    command = var.tfmodule4
-    interpreter = ["/bin/sleep"]
-  }
-}
