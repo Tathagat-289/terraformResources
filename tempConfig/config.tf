96850077fd523cfcf27a7cbe6fed0ba615467893
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2021-05-12 14:09:57 +0530

    use customer tf file

--- tempConfig/config.tf
+++ tempConfig/config.tf
@@ -1,22 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
-      # provider plugins.
-      #
-      # source is not required for the hashicorp/* namespace as a measure of
-      # backward compatibility for commonly-used providers, but recommended for
-      # explicitness.
-      source  = "hashicorp/azurerm"
-      version = "~> 2.12"
-    }
-    newrelic = {
-      # source is required for providers in other namespaces, to avoid ambiguity.
-      source  = "newrelic/newrelic"
-      version = "~> 2.1.1"
+    oktaasa = {
+      source = "oktadeveloper/oktaasa"
     }
   }
+  required_version = ">= 0.14"
 }
+
 
 
 variable "tfmodule4" {}
