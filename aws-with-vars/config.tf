590403aefd99793332d6485710a60213efa35c8c
Tathagat <tathagat289@gmail.com>
2021-12-28 11:34:42 +0530

    Update config.tf

--- aws-with-vars/config.tf
+++ aws-with-vars/config.tf
@@ -7,10 +7,14 @@ variable "tag" {
   default = "terragrunt-bugbash"
 }
 
+variable "region" {
+  default = "us-east-1"
+}
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = "us-east-1"
+  region = var.region
 }
 
 terraform {
