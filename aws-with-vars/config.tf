088fc49dca2345572bf7ce7c93f04bdb41279b59
Tathagat <tathagat289@gmail.com>
2022-05-10 15:07:50 +0530

    add double dollar

--- aws-with-vars/config.tf
+++ aws-with-vars/config.tf
@@ -30,6 +30,68 @@ resource "aws_instance" "ec2_instance_test" {
   }
 }
 
+resource "aws_ssm_document" "LDS-EC2InstallConfigCloudWatch" {
+  name          = "LDS-EC2InstallConfigCloudWatch"
+  document_type = "Command"
+  #replace content
+  content = <<DOC
+{
+  "schemaVersion": "2.2",
+  "description": "InstallAndConfigureCloudWatch",
+  "parameters": {
+    "ParameterStoreConfigName": {
+      "type": "String",
+      "description": "Parameter Store Config Name",
+      "default": "AmazonCloudWatchConfig"
+    }
+  },
+  "mainSteps": [
+    {
+      "action": "aws:configurePackage",
+      "name": "InstallCloudWatchAgent",
+      "inputs": {
+        "name": "AmazonCloudWatchAgent",
+        "action": "Install",
+        "installationType": "Uninstall and reinstall",
+        "version": "",
+        "additionalArguments": "{}"
+      }
+    },
+    {
+      "name": "ConfigureCloudWatchAgent",
+      "action": "aws:runPowerShellScript",
+      "precondition": {
+        "StringEquals": [
+          "platformType",
+          "Windows"
+        ]
+      },
+      "inputs": {
+        "runCommand": [
+          " Set-StrictMode -Version 2.0",
+          " $ErrorActionPreference = 'Stop'",
+          " $Cmd = \"$${Env:ProgramFiles}\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent-ctl.ps1\"",
+          " if (!(Test-Path -LiteralPath \"$${Cmd}\")) {",
+          "     Write-Output 'CloudWatch Agent not installed.  Please install it using the AWS-ConfigureAWSPackage SSM Document.'",
+          "     exit 1",
+          " }",
+          " Set-StrictMode -Off",
+          " exit $LASTEXITCODE"
+        ]
+      }
+    }
+  ]
+}
+DOC
+}
+resource "aws_ssm_association" "LDS-EC2InstallConfigCloudWatch" {
+  name = aws_ssm_document.LDS-EC2InstallConfigCloudWatch.name
+  targets {
+    key    = "tag:linedata.domainjoin"
+    values = ["ldsprod"] #should this reference the domain value from somewhere else?
+  }
+}
+
 output "tag" {
   value = var.tag
 }
