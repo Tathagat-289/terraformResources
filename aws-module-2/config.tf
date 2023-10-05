1210a6be8846d4901b45497edcf0690f49849791
Tathagat <tathagat289@gmail.com>
2023-10-05 18:45:06 +0530

    Update config.tf

--- aws-module-2/config.tf
+++ aws-module-2/config.tf
@@ -10,7 +10,7 @@ variable "tag" {
 provider "aws" {
 #  access_key = var.access_key
 #  secret_key = var.secret_key
-#  region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "ec2_instance_test" {
