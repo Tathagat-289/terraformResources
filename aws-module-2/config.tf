fb4db3914090f9b45ff5fd3d06399780a500e7a1
Tathagat <tathagat289@gmail.com>
2023-10-06 01:26:24 +0530

    Update config.tf

--- aws-module-2/config.tf
+++ aws-module-2/config.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "null_resource" "delaymodule3" {
   provisioner "local-exec" {
-    command = "tfmodule3"
+    command = "echo hello"
     interpreter = ["/bin/sleep"]
   }
 }
