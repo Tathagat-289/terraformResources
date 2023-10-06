cbc7486f174ba1b83160e22c6bc2c4e19174a70c
Tathagat <tathagat289@gmail.com>
2023-10-06 11:49:12 +0530

    Update config.tf

--- module1/config.tf
+++ module1/config.tf
@@ -1,4 +1,15 @@
-resource "local_file" "foomodule1" {
-    content     = "foo2!"
-    filename = "${path.module}/foo1.bar"
+# Define a provider block (optional, depends on your project)
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
+
+# Define a dummy resource
+resource "null_resource" "dummy" {
+  triggers = {
+    always_run = timestamp()  # This trigger ensures the resource runs on every apply
+  }
+
+  provisioner "local-exec" {
+    command = "echo Dummy resource created"
+  }
 }
