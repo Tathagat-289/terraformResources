626571a9ec6df73d1fc31f0883941ef8fcafe8de
Tathagat <tathagat289@gmail.com>
2023-10-10 14:59:50 +0530

    Update config.tf

--- module1/config.tf
+++ module1/config.tf
@@ -6,5 +6,4 @@ provider "aws" {
 resource "aws_instance" "example_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
   instance_type = "t2.micro"              # Replace with your desired instance type
-  key_name      = "your-key-pair-name"    # Replace with your key pair name
 }
